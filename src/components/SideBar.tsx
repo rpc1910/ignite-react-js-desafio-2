aabb12c3fbc1ebb36aa431920d04c88611269a1e
Rodrigo Passos <rodrigo.passos25@gmail.com>
2021-05-16 19:11:27 -0300

    componentização

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,35 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+
+interface GenreResponseProps {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+
+interface SidebarProps {
+  genres: GenreResponseProps[];
+  selectedGenreId: number;
+  onPress(id: number): void;
+}
+
+export function SideBar({ genres, selectedGenreId, onPress }: SidebarProps) {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onPress(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
